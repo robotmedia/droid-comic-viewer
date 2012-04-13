4d595af8f8c905626392b5326cde20b699f01f20
Hermes Pique <hermes.pique@robotmedia.net>
2012-04-13 18:10:23 +0200

    Text & settings changes

--- src/net/robotmedia/acv/ui/settings/AboutSettingsFragment.java
+++ src/net/robotmedia/acv/ui/settings/AboutSettingsFragment.java
@@ -17,13 +17,20 @@ package net.robotmedia.acv.ui.settings;
 
 import net.androidcomics.acv.R;
 import net.robotmedia.acv.Constants;
+import net.robotmedia.acv.ui.SubscribeActivity;
+import net.robotmedia.acv.ui.widget.DialogFactory;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 
 public class AboutSettingsFragment extends ExtendedPreferenceFragment {
 
+	private static final String KEY_SUBSCRIBE = "subscribe";
+	private static final String SOURCE_VALUE = "DroidComicViewer";
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,6 +46,31 @@ public class AboutSettingsFragment extends ExtendedPreferenceFragment {
 			versionName = "1";
 		}
 		version.setSummary(version.getSummary() + versionName);
+		
+		final Preference subscribe = findPreference(KEY_SUBSCRIBE);
+		subscribe.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+			public boolean onPreferenceClick(Preference preference) {
+				Intent myIntent = new Intent(AboutSettingsFragment.this.getActivity(), SubscribeActivity.class);
+				myIntent.putExtra(SubscribeActivity.SOURCE_EXTRA, SOURCE_VALUE);
+				startActivityForResult(myIntent, Constants.SUBSCRIBE_CODE);
+				return true;
+			}
+		});
 	}
 	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == Constants.SUBSCRIBE_CODE) {
+			switch (resultCode) {
+			case -1:
+				DialogFactory.showSimpleAlert(this.getActivity(), true, R.string.dialog_subscribe_success_title, R.string.dialog_subscribe_success_text);
+				break;
+			case SubscribeActivity.RESULT_ERROR:
+				DialogFactory.showSimpleAlert(this.getActivity(), false, R.string.dialog_subscribe_error_title, R.string.dialog_subscribe_error_text);
+				break;
+			}
+		}
+	}
 }
